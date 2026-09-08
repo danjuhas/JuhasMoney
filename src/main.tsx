2b7473672f516e9111eafb947677df392ab2c5a6
Danilo <danilo@Danilos-MacBook-Pro.local>
2026-09-08 20:47:35 +0100

    add sentry on the app

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,32 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
+import * as Sentry from "@sentry/react";
 import App from './App';
 import './index.css';
 import './lib/i18n';
 import { PreferencesProvider } from './contexts/PreferencesContext';
+
+Sentry.init({
+  dsn: "https://df5c5da19b36bb47c76fc8de64729877@o4512052444790784.ingest.de.sentry.io/4512052452524112",
+  dataCollection: {
+    // To disable sending user data and HTTP bodies, uncomment the lines below. For more info visit:
+    // https://docs.sentry.io/platforms/javascript/guides/react/configuration/options/#dataCollection
+    // userInfo: false,
+    // httpBodies: []
+  },
+  integrations: [
+    Sentry.browserTracingIntegration(),
+    Sentry.replayIntegration()
+  ],
+  // Tracing
+  tracesSampleRate: 1.0, //  Capture 100% of the transactions
+  // Set 'tracePropagationTargets' to control for which URLs distributed tracing should be enabled
+  tracePropagationTargets: ["localhost", /^https:\/\/yourserver\.io\/api/],
+  // Session Replay
+  replaysSessionSampleRate: 0.1, // This sets the sample rate at 10%. You may want to change it to 100% while in development and then sample at a lower rate in production.
+  replaysOnErrorSampleRate: 1.0 // If you're not already sampling the entire session, change the sample rate to 100% when sampling sessions where errors occur.
+});
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
